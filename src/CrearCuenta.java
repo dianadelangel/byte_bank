7afd33e85909c183218a9a3710cb0ca951b177e2
Diana Gonzalez <dianita3686@gmail.com>
2023-07-06 22:15:35 -0600

    feat: Se genera la segunda instancia de Cuenta

--- src/CrearCuenta.java
+++ src/CrearCuenta.java
@@ -1,9 +1,12 @@
 public class CrearCuenta {
     public static void main(String[] args) {
         cuenta primeraCuenta = new cuenta();
+        cuenta segundaCuenta = new cuenta();
 
         primeraCuenta.saldo = 256.69;
+        segundaCuenta.saldo = 856.54;
 
         System.out.println(primeraCuenta.saldo);
+        System.out.println(segundaCuenta.saldo);
     }
 }
