daaa935f95c0b7bd120a7d672cc827bbf11f2820
Diana Gonzalez <dianita3686@gmail.com>
2023-07-06 21:40:36 -0600

    feat: Se agrega la instancia de Cuenta

---
+++ src/CrearCuenta.java
@@ -0,0 +1,9 @@
+public class CrearCuenta {
+    public static void main(String[] args) {
+        cuenta primeraCuenta = new cuenta();
+
+        primeraCuenta.saldo = 256.69;
+
+        System.out.println(primeraCuenta.saldo);
+    }
+}
--- src/cuenta.java
+++ src/cuenta.java
@@ -1,3 +1,6 @@
 class cuenta{
-    
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
 }
